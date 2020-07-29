465b494d4039319e1b780f8ce8e1d6f2e6879da7
travisbrigman <travis1000@gmail.com>
2020-07-29 16:03:46 -0500

    switched to API fetch for data

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -1,40 +1,18 @@
-/*
- *   Journal data provider for Daily Journal application
- *
- *      Holds the raw data about each entry and exports
- *      functions that other modules can use to filter
- *      the entries for different purposes.
- */
+let journal = []
 
-// This is the original data.
-const journal = [
-    {
-        id: 1,
-        date: "07/24/2025",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Ok"
-    },
-    {
-        id: 2,
-        date: "07/26/2025",
-        concept: "Javascript Function",
-        entry: "We talked about the single responsibility principle.",
-        mood: "Good"
-    },
-    {
-        id: 3,
-        date: "07/30/2025",
-        concept: "HTML & CSS",
-        entry: "Learned more than I cared to about CSS.",
-        mood: "Tired"
-    }
-]
+export const getJournalEntries = () => {
+    
+    return fetch("http://localhost:3000/journal")
+    .then(response => response.json())
+    .then(ParsedEntries => {
+        journal = ParsedEntries
+    })
 
-/*
-    You export a function that provides a version of the
-    raw data in the format that you want
-*/
+}
+
+
+
+
 export const useJournalEntries = () => {
     const sortedByDate = journal.sort(
         (currentEntry, nextEntry) =>
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
 import { EntryListComponent } from "./JournalEntryList.js";
+import { getJournalEntries } from "./JournalDataProvider.js";
 
 EntryListComponent()
+getJournalEntries()
